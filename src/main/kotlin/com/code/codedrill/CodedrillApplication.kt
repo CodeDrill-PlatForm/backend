51b5a59add73e611116470003054d6dd02537a36
mclub4 <mclub4@kookmin.ac.kr>
2025-07-27 14:45:05 +0900

    project init

---
+++ src/main/kotlin/com/code/codedrill/CodedrillApplication.kt
@@ -0,0 +1,11 @@
+package com.code.codedrill
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodedrillApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodedrillApplication>(*args)
+}
